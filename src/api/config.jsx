af8f35914fbc88e9bc236e9401225e5fd01fe93b
LuisSoraca27 <luisenriquesoraca27@gmail.com>
2025-10-17 16:13:16 -0500

    Update API base URL for production environment in config.jsx

--- src/api/config.jsx
+++ src/api/config.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 // const NODE_ENV = import.meta.env.VITE_NODE_ENV || 'development'
 
 // const baseURL = NODE_ENV === 'development' ? import.meta.env.VITE_URLDEV : import.meta.env.VITE_URLPROD
- const baseURL = "https://servidor-movieplay-production.up.railway.app/api/v1/"
-//  const baseURL = "http://localhost:4002/api/v1/"
+ const baseURL = "https://web-di72og4j1xut.up-de-fra1-k8s-1.apps.run-on-seenode.com/api/v1/"
+  // const baseURL = "http://localhost:4002/api/v1/"
 const dksoluciones = axios.create({
     baseURL,
 });
